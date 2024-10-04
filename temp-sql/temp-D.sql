b6070018e1381427a2f3ed57ca56bb24e31bf6d6
Dilusha Madushan <164498659+dilushamadushan@users.noreply.github.com>
2024-10-04 22:38:46 +0530

    some changes

--- temp-sql/temp-D.sql
+++ temp-sql/temp-D.sql
@@ -1,38 +1,38 @@
 --Crete database
 
-CREATE DATABASE tecmis;
+CREATE DATABASE teamZero;
 
 --Create Tables
 
 CREATE TABLE 
     Student(   
         student_id VARCHAR(6) PRIMARY KEY, 
-        nic VARCHAR(15),
+        nic CHAR(12),
         state VARCHAR(30),
-        dep_id CHAR(5)
+        dep_id CHAR(4)
     );
 
 CREATE TABLE 
     Lecture(
-        lecture_id VARCHAR(6) PRIMARY KEY,
-        nic VARCHAR(15),
+        lecture_id VARCHAR(5) PRIMARY KEY,
+        nic CHAR(12),
         position VARCHAR(20),
-        dep_id CHAR(5)
+        dep_id CHAR(4)
     );
 
 CREATE TABLE 
     Course(
-        course_code char(10) PRIMARY KEY,
+        course_code char(8) PRIMARY KEY,
         course_name varchar(20),
-        course_credit int,
-        course_houre int,
+        course_credit INT,
+        course_houre INT,
         course_type varchar(15) NOT NULL,
-        dep_id char(5)
+        dep_id CHAR(4)
     );
 
 CREATE TABLE 
     Lecture_Course(
-        lecture_id VARCHAR(6),
-        course_code char(10)
+        lecture_id VARCHAR(5),
+        course_code CHAR(8)
     );
 
