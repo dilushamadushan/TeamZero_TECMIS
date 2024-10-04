7e56d27e86c7ebee3009c1b4dd97639e03723e05
Dilusha Madushan <164498659+dilushamadushan@users.noreply.github.com>
2024-10-04 16:25:20 +0530

    created lecture table

--- temp-sql/temp-D.sql
+++ temp-sql/temp-D.sql
@@ -10,4 +10,13 @@ CREATE TABLE
         nic VARCHAR(15),
         state VARCHAR(30),
         dep_id CHAR(5)
-        );
+    );
+
+CREATE TABLE 
+    Lecture(
+        lecture_id VARCHAR(6) PRIMARY KEY,
+        nic VARCHAR(15),
+        position VARCHAR(20),
+        dep_id CHAR(5)
+    );
+
