ef75d400de2061b75220cff54fe9ce6c26cec348
ishan <164498170+ishansivmal@users.noreply.github.com>
2024-10-05 22:42:49 +0530

    added  Mark table

--- temp-sql/temp-I.sql
+++ temp-sql/temp-I.sql
@@ -8,3 +8,21 @@ CREATE TABLE Admin
      admin_id VARCHAR(10) PRIMARY KEY,
      role  VARCHAR(10)
 );
+
+-- create table mark--
+
+CREATE TABLE Mark
+(
+    mark_id CHAR(10) PRIMARY KEY,
+    quiz_1 INT,
+    quiz_2 INT,
+    quiz_3 INT,
+    assesment INT,
+    mid_theory INT,
+    mid_practical INT,
+    end_theory INT,
+    end_practical INT,
+    student_id VARCHAR(6),
+    course_code char(8)
+);
+
