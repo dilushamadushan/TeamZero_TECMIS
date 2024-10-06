53e84e3e76daa3ae3254d67f84b1a0001818779d
Nimhara Chalana <162380399+nimharachalana@users.noreply.github.com>
2024-10-06 16:07:49 +0530

    created attendence table

--- temp-sql/temp-N.sql
+++ temp-sql/temp-N.sql
@@ -7,3 +7,15 @@ CREATE TABLE Technical_officer(
     nic CHAR(12),
     role VARCHAR(20),
 );
+
+-- create table Attendence--
+
+CREATE TABLE Attendence(
+    data DATE,
+    att_state VARCHAR(20),
+    session_type VARCHAR(25),
+    student_id VARCHAR(6),
+    medical_id CHAR(10),
+    tech_officer_id VARCHAR(6),
+    course_code CHAR(8)
+);
